3ce3498019076fb70840c1bb44a673c08a4678a4
Shallen-99 <shallenburks@gmail.com>
2026-02-19 08:40:06 -0500

    setting up my deployment

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      process.env.FRONTEND_URL
+    ].filter(Boolean),
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 // Serve uploaded files
